bbe370395b481d8ab4370cb28c4c79815409c8e5
Colin Watson <cjwatson@debian.org>
2010-10-12 01:06:05 +0100

    * COPYING: New file. * autogen.sh: Add a copyright/licence notice. * configure.ac: Likewise. * lib/pipeline-private.h: Update copyright years.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,15 @@
 #! /bin/sh -e
 
+# Copyright (C) 2010 Colin Watson.
+#
+# This file is free software; the author gives unlimited permission to copy
+# and/or distribute it, with or without modifications, as long as this
+# notice is preserved.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 if type gnulib-tool >/dev/null 2>&1; then
 	gnulib-tool --update >/dev/null
 	patch -s -p0 < gnulib/lib/xmalloc.patch
